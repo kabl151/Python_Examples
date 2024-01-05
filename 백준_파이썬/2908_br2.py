fe5bee4a3f0f505dc7527cdbb5983985ea3675b8
kabl151 <137067358+kabl151@users.noreply.github.com>
2024-01-05 08:32:19 +0000

    2908_풀이중

---
+++ 백준_파이썬/2908_br2.py
@@ -0,0 +1,24 @@
+print(" -----------------------------------")
+print(" -=-= P R O G R A M _ S T A R T =-=-")
+print("▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼")
+######################################################
+
+A, B = list(map(int, input().split()))
+
+list_A = []
+for a in int(A):
+    list_A.append(a)
+    
+list_B = []
+for b in (B):
+    list_B.append(b)
+    
+list_A.reverse()
+list_B.reverse()
+
+
+
+#######################################################
+print(" ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲")
+print(" -=-=   P R O G R A M _ E N D   =-=-")
+print(" -----------------------------------")
