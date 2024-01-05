46fa259824f9a1f3e88f0a5095c1d683a9276019
kabl151 <kabl151@naver.com>
2024-01-05 21:34:50 +0900

    2675_24.01.05_solved

---
+++ 백준_파이썬/2675_br2.py
@@ -0,0 +1,21 @@
+print(" ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+print(" -=-= P R O G R A M _ S T A R T =-=-")
+print("▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽")
+######################################################
+
+T = int(input())
+result_Str = ""
+
+for t in range(T):
+    R, S = map(str, input().split() )
+    lenn_S = len(S)
+    for s in range(lenn_S):
+        result_Str = result_Str + int(R)*S[s]
+        
+    print(result_Str)
+    result_Str = ""
+
+#######################################################
+print(" △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲")
+print(" -=-=   P R O G R A M _ E N D   =-=-")
+print(" ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
