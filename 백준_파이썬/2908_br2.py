7c908cd8a97bb438fa1dadf1e620d27fb147b46a
kabl151 <kabl151@naver.com>
2024-01-05 21:11:38 +0900

    2908_solved

--- 백준_파이썬/2908_br2.py
+++ 백준_파이썬/2908_br2.py
@@ -6,15 +6,24 @@ print("▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ �
 A, B = list(map(int, input().split()))
 
 list_A = []
-for a in int(A):
+for a in str(A):
     list_A.append(a)
     
 list_B = []
-for b in (B):
+for b in str(B):
     list_B.append(b)
-    
+
 list_A.reverse()
 list_B.reverse()
+
+reversed_A = int(list_A[0])*100 + int(list_A[1])*10 + int(list_A[2])
+reversed_B = int(list_B[0])*100 + int(list_B[1])*10 + int(list_B[2])
+
+if reversed_A > reversed_B:
+    print(reversed_A)
+else:
+    print(reversed_B)
+    
 
 
 
