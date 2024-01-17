3e1f9747ac61177006b1290a5ee52e984ce97aa6
kabl151 <kabl151@naver.com>
2024-01-17 17:46:45 +0900

    test

--- 백준_파이썬/테스트용_2.py
+++ 백준_파이썬/테스트용_2.py
@@ -1,20 +1,55 @@
-N = int(input())
-k = 0
-# N =5x + 3y
-num_divided_5 = N // 5
-num_divided_3 = (N - (5 * num_divided_5)) // 3
-num_divided_3_First = N // 3
+number_of_people = 0
+userInfo = {}
+userDB = []
+number_of_book = 100
 
-if N - ((5 * num_divided_5) + (3 * num_divided_3)) == 0:
-    print(num_divided_3 + num_divided_5)
-else: # 5부터 조졌는데 안 될 때.
-    if N - 3 * num_divided_3_First == 0:
-        print(num_divided_3_First)
-    else:
-        for t in range(1000):
-            if (N - (5 * t)) >= 0:
-                if (N - (5 * t))%3 == 0:
-                    k = (N - (5 * t)) // 3
-                    print( t + k )
-                    break
-        print(-1)
+name = ['김시습', '허균', '남영로', '임제', '박지원']
+age = [20, 16, 52, 36, 60]
+address = ['서울', '강릉', '조선', '나주', '한성부']
+
+
+def increase_user():
+    global number_of_people
+    number_of_people += 1
+    return number_of_people
+    
+
+
+def create_user():
+    global userInfo
+    global name
+    global age
+    global address
+    for i in range(len(name)):
+        user_name = name[i]
+        user_age = age[i]
+        user_address = address[i]
+        print(f'{name[i]}님 환영합니다!')
+        userInfo = dict(name = user_name, age = user_age, adress = user_address)
+        
+        userDB_done = userDB.append(userInfo)
+    return userDB
+
+
+many_user = create_user()
+
+
+
+def rental_book(bookEa):
+    global number_of_book
+    for i in range(len(name)):
+        decrease_book(bookEa)
+        print(f'남은 책의 수 : {number_of_book}')
+        
+    
+        print(f'{many_user[i][name]}님이 {bookEa}권의 책을 대여하였습니다.')
+        print(f'남은 책의 수 : {number_of_book}')
+        pass
+
+def decrease_book(bookEa):
+    global number_of_book
+    number_of_book -= bookEa
+    
+
+
+create_user()
