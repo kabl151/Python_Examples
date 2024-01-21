8cfcbba27fa4f329ef5659ee5501d9604a6de91a
kabl151 <kabl151@naver.com>
2024-01-22 00:46:31 +0900

    0122

---
+++ 백준_파이썬/연습장/Free_test.py
@@ -0,0 +1,9 @@
+vowels = 'aeiou'
+
+print(('a' and 'b') in vowels) #False
+print(('b' and 'a') in vowels) #True
+
+print(('a' or 'b') in vowels) #True
+print(('b' or 'a') in vowels) #False
+
+print(3 and 5)
