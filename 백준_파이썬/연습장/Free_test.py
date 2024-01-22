c08a5d390e86fdcf8ce8d493549e6c8984b98b2b
kabl151 <kabl151@naver.com>
2024-01-22 17:54:31 +0900

    0122

--- 백준_파이썬/연습장/Free_test.py
+++ 백준_파이썬/연습장/Free_test.py
@@ -1,9 +1,35 @@
-vowels = 'aeiou'
+"""
+<문제 링크>
+    https://www.acmicpc.net/problem/1436
+    
+<풀이 링크>
+    https://wikidocs.net/206249
 
-print(('a' and 'b') in vowels) #False
-print(('b' and 'a') in vowels) #True
+#----------------------------------------#
+#             S U D O _ C O D E          #
+#----------------------------------------#
 
-print(('a' or 'b') in vowels) #True
-print(('b' or 'a') in vowels) #False
 
-print(3 and 5)
+
+
+"""
+#-----------------------------------------------------
+print(" ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+print(" -=-= P R O G R A M _ S T A R T =-=-")
+print("▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽ ▼▽")
+######################################################
+str_temp = 'askjdkjhdsfkjh askldflk lksdalkjfsad'
+
+for i in range(len(a)):
+    if str_temp[i] == " ":
+        print(" ")
+
+
+######################################################
+print(" △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲ △ ▲")
+print(" -=-=   P R O G R A M _ E N D   =-=-")
+print(" ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+""" Comment
+loop 종료가 해결이 안되어서 sys.exit()를 사용했음
+비둘기 머리콥터 문제인걸까?
+"""
---
+++ 백준_파이썬/연습장/연습장.py
@@ -0,0 +1,9 @@
+# 아래 함수를 수정하시오.
+def capitalize_words(word):
+    word.upper() #사용  # 대문자로 변환 후 반환.
+    #단어의 구분 -> 띄어쓰기 있으면 그 다음 글자를 타겟.
+    
+    
+
+result = capitalize_words("hello, world!")
+print(result)
