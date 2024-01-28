884d338f78b003e946a697c24373d5cba5e5e06a
kabl151 <kabl151@naver.com>
2024-01-29 02:41:33 +0900

    done.

--- 1차_월말평가_대비/십자뒤집기.py
+++ 1차_월말평가_대비/십자뒤집기.py
@@ -9,18 +9,23 @@ N = int(input())
 for n in range(N):
     X, Y = map(int, input().split())
     #가로줄 바꾸기
-    for k in range(1,19):
-        if batoLst[X][k] == 1:
-            batoLst[X][k] = 0
-        else:
-            batoLst[X][k] = 1
+    for k in range(19):
+        if batoLst[X-1][k] == 1:
+            batoLst[X-1][k] = 0
+            
+        elif batoLst[X-1][k] == 0:
+            batoLst[X-1][k] = 1
             
     #세로줄 바꾸기
-    for j in range(1,19):
-        if batoLst[j][Y] == 1:
-            batoLst[j][Y] = 0
-        else:
-            batoLst[j][Y] = 1
+    for j in range(19):
+        if batoLst[j][Y-1] == 1:
+            batoLst[j][Y-1] = 0
             
-for i in range(1,19):
+        elif batoLst[j][Y-1] == 0:
+            batoLst[j][Y-1] = 1
+            
+for i in range(19):
     print(*batoLst[i])
+    
+# for i in range(1,20):
+#     print(*batoLst[i])
--- 백준_파이썬/연습장/테스트용_3.py
+++ 백준_파이썬/연습장/테스트용_3.py
@@ -1,25 +1,22 @@
-food_list = [
-    {
-        '종류': '한식',
-        '이름': '잡채'
-    },
-    {
-        '종류': '채소',
-        '이름': '토마토'
-    },
-    {
-        '종류': '중식',
-        '이름': '자장면'
-    },
-]
-
-# 아래에 코드를 작성하시오.
-for i in range(3):
-    if food_list[i]['이름'] == '토마토':
-        food_list[i]['종류'] = '과일'
-        print(f"{food_list[i]['이름']} 은/는 {food_list[i]['종류']} (이)다.")
+# 6. 리스트에서 중복되어 있지 않은 요소 반환(하나 반환, 여러개 반환)
+def no_double(origin_list):
+    count_Dict = {} #딕셔너리에 넣는 방법
+    for i in origin_list:
+        count_Dict[i] = count_Dict.get(i, 0) + 1 #카운트 딕셔너리에 i라는 키값이 없다면 0을 세팅해주지만,
+                                                #기본적으로 하나가 나오기 때문에 1을 더해줌.
     
-    elif food_list[i]['이름'] == '자장면':
-        print('자장면엔 고춧가루지')
-        print(f"{food_list[i]['이름']} 은/는 {food_list[i]['종류']} (이)다.")
-print(food_list)
+    no_Dupli_List = [] #한 번만 등장한 숫자를 받아올 리스트
+    for key, value in count_Dict.items(): #딕셔너리에 키와 밸류를 받아옴.
+        if value == 1: #그 중에 밸류값이 1이라면
+            no_Dupli_List.append(key)   #리스트에 키 값을 추가!
+            
+    return no_Dupli_List
+
+    # list_copy = origin_list[:]
+    # list_copy.sort()
+    # for i in list_copy:
+    #     if i not in origin_list:
+    #         return i
+        
+a_list = [1,2,2,3,3,4,4,5]
+print(no_double(a_list))
