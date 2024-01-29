743757a092e43c75ef73a02afe071022f6177f30
kabl151 <kabl151@naver.com>
2024-01-30 00:32:51 +0900

    cmtttt

--- 백준_파이썬/골드/2447_별찍기10_gl5.py
+++ 백준_파이썬/골드/2447_별찍기10_gl5.py
@@ -1,25 +1,51 @@
-N = 3
-def PEKA_1(n):
-    if n == 3:
-        return '***'
-    else:
-        return f'{PEKA_1(n/3)*3}'
+def draw_StarS(n):
+    if n == 1:
+        return ['*']
+
+    starsss = draw_StarS(n//3)
+    L = []
     
-def PEKA_2(n):
-    if n == 3:
-        return '* *'
-    else:
-        return f'{PEKA_2(n/3)} {PEKA_2(n/3)}'
+    for star in starsss:
+        L.append(star*3)
+    for star in starsss:
+        L.append(star+' '*(n//3)+star)
+    for star in starsss:
+        L.append(star*3)
+    
+    return L
 
-
-def sTarK(n):
-    if n == 3:
-        return "***\n* *\n***"
-    else:
-        return f'{PEKA_1(n)}\n{PEKA_2(n)}\n{PEKA_1(n)}'
-
-print(sTarK(9))
+N = int(input())
+print('\n'.join(draw_StarS(N)))
 
 
 
-N_list = list(map(int, input().split()))
+
+
+
+
+
+
+
+# N = 3
+# def PEKA_1(n):
+#     if n == 3:
+#         return '***'
+#     else:
+#         return f'{PEKA_1(n/3)*3}'
+    
+# def PEKA_2(n):
+#     if n == 3:
+#         return '* *'
+#     else:
+#         return f'{PEKA_2(n/3)*3}'
+
+
+# def sTarK(n):
+#     if n == 3:
+#         # return f'***''\n'f'* *''\n'f'***' 
+#         return f'{PEKA_1(n)}{PEKA_2(n)}{PEKA_1(n)}'
+#     else:
+#         tick = f'{sTarK(n/3)*int(n/3)}'
+#         return tick
+
+# print(sTarK(9))
